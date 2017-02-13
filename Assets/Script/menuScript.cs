5ea7bf2a46fe50635fe0f10d5edef687eff31a3c
ajizi <hellqvistmarcus@gmail.com>
2017-02-13 14:16:34 +0100

    WORKING?

---
+++ Assets/Script/menuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("pickLevel");        
+    }
+
+    public void levelOne()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+}
