c642115b269f601828d746c2304ca57ffa964b0f
ajizi <hellqvistmarcus@gmail.com>
2017-03-01 16:10:40 +0100

    tutorial stuff almost not working

--- Assets/Script/Button.cs
+++ Assets/Script/Button.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour {
 
     public int boxValue = 0;
     public int myID;
+    public tutorialScript ts;
     public gameManager gm;
     public GameObject isActive;
-
+    bool canClick;
+    public Scene CurrentScene;
     // Use this for initialization
     void Start () {
+
+        CurrentScene = SceneManager.GetActiveScene();
+        Debug.Log(CurrentScene.name);
+
+        if (!CurrentScene.name.Contains("tutorial"))
+            canClick = true;
+        else
+            canClick = false;
     }
 	
 	// Update is called once per frame
@@ -19,14 +30,25 @@ public class Button : MonoBehaviour {
 
     void OnMouseDown()
     {
-        if(gm.buttonValue[myID - 1] == 0){
-            gm.buttonValue[myID - 1] = 1;
-            isActive.SetActive(true);
-        }
-        else
+        if (canClick)
         {
-            gm.buttonValue[myID - 1] = 0;
-            isActive.SetActive(false);
+            if (gm.buttonValue[myID - 1] == 0)
+            {
+                gm.buttonValue[myID - 1] = 1;
+                isActive.SetActive(true);
+            }
+            else
+            {
+                gm.buttonValue[myID - 1] = 0;
+                isActive.SetActive(false);
+            }
+        }
+    }
+
+    void OnMouseOver() {
+        if (isActive.activeSelf)
+        {
+            Debug.Log("HEJ");
         }
     }
 }
---
+++ Assets/Script/tutorialScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class tutorialScript : MonoBehaviour {
+
+    public int[] buttonValue;
+    public string[] alphabet;
+    public int[,] brailleCharacters;
+    private Text activeBrailleText;
+    public GameObject[] boxes;
+
+
+    // Use this for initialization
+    void Start () {
+        //Player Array
+        buttonValue = new int[6] { 0, 0, 0, 0, 0, 0 };
+        //Array
+        alphabet = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+        //Database Array
+        brailleCharacters = new int[26, 6] {
+            { 1, 0, 0, 0, 0, 0 }, // Bokstaven A
+            { 1, 1, 0, 0, 0, 0 }, // Bokstaven B
+            { 1, 0, 0, 1, 0, 0 }, // Bokstaven C
+            { 1, 0, 0, 1, 1, 0 }, // Bokstaven D
+            { 1, 0, 0, 0, 1, 0 }, // Bokstaven E
+            { 1, 1, 0, 1, 0, 0 }, // Bokstaven F
+            { 1, 1, 0, 1, 1, 0 }, // Bokstaven G
+            { 1, 1, 0, 0, 1, 0 }, // Bokstaven H
+            { 0, 1, 0, 1, 0, 0 }, // Bokstaven I
+            { 0, 1, 0, 1, 1, 0 }, // Bokstaven J
+            { 1, 0, 1, 0, 0, 0 }, // Bokstaven K
+            { 1, 1, 1, 0, 0, 0 }, // Bokstaven L
+            { 1, 0, 1, 1, 0, 0 }, // Bokstaven M
+            { 1, 0, 1, 1, 1, 0 }, // Bokstaven N
+            { 1, 0, 1, 0, 1, 0 }, // Bokstaven O
+            { 1, 1, 1, 1, 0, 0 }, // Bokstaven P
+            { 1, 1, 1, 1, 1, 0 }, // Bokstaven Q
+            { 1, 1, 1, 0, 1, 0 }, // Bokstaven R
+            { 0, 1, 1, 1, 0, 0 }, // Bokstaven S
+            { 0, 1, 1, 1, 1, 0 }, // Bokstaven T
+            { 1, 0, 1, 0, 0, 1 }, // Bokstaven U
+            { 1, 1, 1, 0, 0, 1 }, // Bokstaven V
+            { 0, 1, 0, 1, 1, 1 }, // Bokstaven W
+            { 1, 0, 1, 1, 0, 1 }, // Bokstaven X
+            { 1, 0, 1, 1, 1, 1 }, // Bokstaven Y
+            { 1, 0, 1, 0, 1, 1 } // Bokstaven Z
+        };
+
+        activeBrailleText = GameObject.Find("activeBrailleCharacter").GetComponent<Text>();
+        activeBrailleText.text = "A";
+
+        ShowLetter();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void ShowLetter()
+    {
+        boxes[0].SetActive(true);
+    }
+}
